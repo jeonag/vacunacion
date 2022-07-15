7ee549b33fe8b532d74fdd022ebd062fa025cca3
Jhon Ona <jona@ec.krugercorp.com>
2022-07-15 15:47:13 -0500

    add entity persona

---
+++ src/main/java/com/kruger/vacunacion/empleados/entidad/Persona.java
@@ -0,0 +1,19 @@
+package com.kruger.vacunacion.empleados.entidad;
+
+public class Persona {
+
+    private Integer catalogTypeCode;
+    private String catalogValueName;
+    private Long numericValue;
+    private String validationLevel;
+    private String order;
+    private String status;
+    private String defaultValue;
+    private String visible;
+    private String externalCode;
+    private String color;
+    private String shortName;
+    private String description;
+    private String dataType;
+    private String registerUser;
+}
